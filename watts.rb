ab1df7dbe4b43512f67ec314860fa2fab03958ee
Athitya Kumar <athityakumar@gmail.com>
2017-04-16 00:50:14 +0530

    Adds more fields to the CSV files

--- watts.rb
+++ watts.rb
@@ -26,15 +26,19 @@ def bruteforce
     MATERIALS.each do |mat|
         filename = "#{mat[:name]}.csv"
         File.delete(filename) if File.exists? filename
-        csv_write(filename,"#{L_RANGE[0]} < L < #{L_RANGE[1]} (in m)","#{D_RANGE[0]} < D < #{D_RANGE[1]} (in m)","#{P_RANGE[0]} < P_allowable < #{P_RANGE[1]} (in kN)","Max. objective = P_allowable / Volume = P/(D*D*L)")   
-        e = mat[:value]
+        csv_write(filename,"#{L_RANGE[0]} < L < #{L_RANGE[1]} (in m)","#{D_RANGE[0]} < D < #{D_RANGE[1]} (in m)","Factor of safety","#{P_RANGE[0]} < P_allowable < #{P_RANGE[1]} (in kN)","Density (g/cc)","Volume (mm^3)","Mass (kg)","Max. objective = P_allowable / Volume")   
+        e = mat[:youngs_modulus]
+        rho = mat[:density]
         data = []
         get_all_steps(L_RANGE,L_STEP).each do |l| # Loop through length
             get_all_steps(D_RANGE,D_STEP).each do |d| # Loop through diameter
-                p = (3.14*3.14*3.14*e*d*d*d*d)/(64*l*l*5) # Apply formula for load
-                obj = p/(l*d*d) # Objective function to be maximized
+                p_cr = (3.14*3.14*3.14*e*d*d*d*d)/(64*l*l) # Apply formula for critical load
+                p = p_cr / FACTOR_OF_SAFETY # Calculate allowable load from critical load
+                v = 3.14 * d * d * l /4
+                m = rho * v
+                obj = p / v # Objective function to be maximized
                 data.push({e: e,l: l,d: d,p: p,obj: obj})
-                csv_write(filename,l,d,p,obj)
+                csv_write(filename,l,d,FACTOR_OF_SAFETY,p,rho*1000000,v,m,obj)
             end
         end 
         
@@ -56,7 +60,8 @@ L_RANGE = [200.0/1000.0,250.0/1000.0]
 D_RANGE = [15.0/1000.0,20.0/1000.0]
 L_STEP = 1.0/1000.0
 D_STEP = 0.1/1000.0
-MATERIALS = [{name: "Aluminium",value: 70.0*1000000000},{name: "Carbon_Steel_Alloy",value: 203.0*1000000000}]
+FACTOR_OF_SAFETY = 5.0
+MATERIALS = [{name: "Aluminium",youngs_modulus: 70.0*1000000000, density: 2.7/1000000},{name: "Carbon_Steel_Alloy",youngs_modulus: 203.0*1000000000, density: 7.85/1000000}]
 
 # Perform the bruteforce
 bruteforce()
